7bb516bf8c393c2005d8ff571744ba9c954ea7d3
Ernesto López <ernesto.lopez@est.fi.uncoma.edu.ar>
2024-07-17 14:54:06 -0300

    ahora recuperar devuelve todos los partidos disputados entre esos dos equipos

--- src/Estructuras/TablaHash.java
+++ src/Estructuras/TablaHash.java
@@ -14,20 +14,21 @@ public class TablaHash {
     }
 
     public boolean insertar(Partido elem) {
-        boolean exit = false;
+        boolean exit = false, encontrado = false;
         int pos = elem.hashCode() % 37;
         Nodo aux = arreglo[pos];
-
-        while (!exit && aux != null) {
-            exit = aux.getElem().equals(elem);
+        while (!encontrado && aux != null) {
+            if (aux.getElem().equals(elem))
+                encontrado = true;
             aux = aux.getEnlace();
         }
         // Si no lo encontro lo inserta, enlazandolo con el que estaba antes en esa pos
-        if (!exit) {
+        if (!encontrado) {
             arreglo[pos] = new Nodo(elem, arreglo[pos]);
             cant++;
+            exit = true;
         }
-        return !exit;
+        return exit;
     }
 
     /*
@@ -36,21 +37,43 @@ public class TablaHash {
      * 
      * Obs: como maximo dos equipos se enfrentan dos veces
      */
-    public String recuperar(Equipo uno, Equipo dos) {
-        Partido p = new Partido(uno, dos, null);
+    public String recuperar(Equipo uno, Equipo dos, String ronda) {
+        Partido p = new Partido(uno, dos, ronda);
         String ret = "No hay partidos entre esos dos equipos";
         // Como el hashcode se usa solo con el nombre
         int pos = p.hashCode() % 37;
-        if (arreglo[pos] != null) {
+        Nodo aux = arreglo[pos];
+        if (aux != null) {
             ret = "";
-            p = (Partido) arreglo[pos].getElem();
-            if (arreglo[pos].getEnlace() != null) {
-                Partido x = (Partido) arreglo[pos].getEnlace().getElem();
-                ret += x.toString() + "\n";
+            String otro = aux.getElem().toString() + "\n";
+            p = aux.getElem();
+            Partido x;
+            while (aux.getEnlace() != null) {
+                x = aux.getEnlace().getElem();
+                otro += x.toString() + "\n";
+                if (x.verifPartido(p)) {
+                    ret += x.toString() + "\n";
+                }
+                aux = aux.getEnlace();
             }
+            System.out.println("------");
+            System.out.println(otro);
+            System.out.println("------");
             ret += p.toString();
         }
         return ret;
+    }
+
+    /*
+     * Para el test
+     */
+    public int recuperarTest(String primero, String segundo) {
+        Equipo uno = new Equipo(primero);
+        Equipo dos = new Equipo(segundo);
+        Partido p = new Partido(uno, dos, null);
+        // Como el hashcode se usa solo con el nombre
+        int pos = p.hashCode() % 37;
+        return pos;
     }
 
     public boolean eliminar(Partido elem) {
